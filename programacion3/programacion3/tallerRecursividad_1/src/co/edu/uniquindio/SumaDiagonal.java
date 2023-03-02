81146a6152bc0a69b7fee3825079b56d6cc4c9c4
Omar Piedrahita <omar.piedrahitat@gmail.com>
2023-03-02 14:52:35 -0500

    Completo ejercicio de suma diagonal con diagonal principal y diagonal secundaria

--- programacion3/programacion3/tallerRecursividad_1/src/co/edu/uniquindio/SumaDiagonal.java
+++ programacion3/programacion3/tallerRecursividad_1/src/co/edu/uniquindio/SumaDiagonal.java
@@ -5,24 +5,42 @@ public class SumaDiagonal {
 
         //1. Sumar la diagonal de una matriz de forma recursiva. Avanzando en filas y columnas
 
-        int [][] matriz = {{1,2,3},{4,5,6},{7,8,9}};
-        int sumaDiagonal = sumarDiagonalMatriz(matriz, 0, 0);
-        System.out.println(sumaDiagonal);
-        System.out.println(matriz.length + ", " + matriz[0].length);
+        int[][] matriz = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int sumaDiagonalPrincipal = sumarDiagonalPrincipal(matriz, 0, 0);
+        System.out.println("Suma principal: " + sumaDiagonalPrincipal);
 
-        }
+        //Sumar la diagonal de una matriz de forma recursiva. Retrocediendo en filas y avanzando en columnas
+        int sumaDiagonalSecundaria = sumarDiagonalSecundaria(matriz, matriz.length-1, 0);
+        System.out.println("Suma secundaria: " + sumaDiagonalSecundaria);
 
-    public static int sumarDiagonalMatriz(int[][] matrix, int i, int j) {
+    }
+
+    public static int sumarDiagonalPrincipal(int[][] matrix, int i, int j) {
         // Caso base: cuando llegamos al final de la matriz
         if (i == matrix.length || j == matrix[0].length) {
             return 0;
         }
         // Caso recursivo: si estamos en la diagonal, sumamos el valor y avanzamos en diagonal
         if (i == j) {
-            return matrix[i][j] + sumarDiagonalMatriz(matrix, i+1, j+1);
+            return matrix[i][j] + sumarDiagonalPrincipal(matrix, i + 1, j + 1);
         }
         // Caso recursivo: si no estamos en la diagonal, avanzamos en la fila
-        return sumarDiagonalMatriz(matrix, i+1, j);
+        return sumarDiagonalPrincipal(matrix, i + 1, j);
+
+
+    }
+
+    public static int sumarDiagonalSecundaria(int[][] matrix, int i, int j) {
+        // Caso base: cuando llegamos al final de la matriz
+        if (i < 0 || j == matrix[0].length) {
+            return 0;
+        }
+        // Caso recursivo: si estamos en la diagonal secundaria, sumamos y restamos para avanzar
+        if ((i + j) == matrix.length-1) {
+            return matrix[i][j] + sumarDiagonalSecundaria(matrix, i - 1, j + 1);
+        }
+        // Caso recursivo: si no estamos en la diagonal, retrocedemos en la fila
+        return sumarDiagonalSecundaria(matrix, i - 1, j);
 
 
     }
