49e275dbbfed9b305e69c5d6aefd2549d0f1a283
Omar Piedrahita <omar.piedrahitat@gmail.com>
2023-03-02 12:59:46 -0500

    Termino ejercicio de invertir palabra

--- programacion3/programacion3/tallerRecursividad_1/src/co/edu/uniquindio/InvertirPalabra.java
+++ programacion3/programacion3/tallerRecursividad_1/src/co/edu/uniquindio/InvertirPalabra.java
@@ -1,2 +1,25 @@
-package co.edu.uniquindio;public class InvertirPalabra {
+package co.edu.uniquindio;
+
+public class InvertirPalabra {
+
+    public static void main(String[] args) {
+
+        //2. Invertir una palabra de forma recursiva. Invertir cuando se deuelva la recursión.
+
+        String word = "uniquindio";
+        String reverseWord = invertirPalabra(word);
+        System.out.println(reverseWord);
+
+    }
+        public static String invertirPalabra(String palabra) {
+            //Caso base
+            if (palabra.length() == 0) {
+                return palabra;
+            } else {
+                //Caso recursivo
+                return invertirPalabra(palabra.substring(1)) + palabra.charAt(0);
+            }
+        }
+
+
 }
